550ebbc987024a9d801ed66e39ddc70c47242e69
Joel Reyes <jreyesguerrero@deloitte.com>
2024-09-17 15:56:49 -0600

    Added database configurations
    Modified DatabaseContext class for the correct injection of configurations.
    Added DatabaseContext to the web project.

--- Practice5_DataAccess/Data/DatabaseContext.cs
+++ Practice5_DataAccess/Data/DatabaseContext.cs
@@ -3,22 +3,11 @@ using Practice5_Model.Models;
 
 namespace Practice5_DataAccess.Data
 {
-    public class DatabaseContext : DbContext
+    public class DatabaseContext(DbContextOptions options) : DbContext(options)
     {
         public DbSet<Product> Product { get; set; }
         public DbSet<Purchase> Purchase { get; set; }
         public DbSet<Sale> Sale { get; set; }
         public DbSet<Inventory> Inventory { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(
-                "Server=USQROJREYESGUE1;" +
-                "Database=Practice5;" +
-                "User Id=admin;" +
-                "Password=root123;" +
-                "Trusted_Connection=True;" +
-                "TrustServerCertificate=True;");
-        }
     }
 }
